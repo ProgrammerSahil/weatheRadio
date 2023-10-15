b624769f9a3cc33179aa73ce641017dbc536d146
ProgrammerSahil <74780160+ProgrammerSahil@users.noreply.github.com>
2023-10-15 22:42:20 +0530

    Delete WeatheRadio directory

--- WeatheRadio/main_page/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from . import complete_Implimentation
-import time
-
-def index(request):
-    song = complete_Implimentation.getSong()
-    return render(request, 'index.html', context={"song_link": "https://open.spotify.com/embed/track/"+song+"?utm_source=generator"})
-
